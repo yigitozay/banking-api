009e7e66e4bd41f89be092a8e330f4434f17904f
Yigit Ozay <yigit.ozay344@gmail.com>
2025-03-29 21:08:12 +0100

    Initial Spring Boot setup with core dependencies

---
+++ src/main/java/com/example/demo/BankTransactionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionAppApplication.class, args);
+	}
+
+}
